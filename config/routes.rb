908625863fbf8831c47bdd0dfbca934a27b33d06
tanakashun <michael021212@gmail.com>
2022-03-29 19:24:02 +0900

    課題5、検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'relationships/follows'
-  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  get '/search', to: 'searches#search'
 
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
